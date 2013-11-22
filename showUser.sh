2bf7a348af8c982a403fbca08b298305b96ac12b
王宇铖 <supermaxxx@sina.com>
2013-11-23 01:09:39 +0800

    Update showUser.sh

--- showUser.sh
+++ showUser.sh
@@ -3,4 +3,4 @@ echo -n "Please enter the user: "
 read user
 cat /etc/passwd|awk -F ":" -v var=$user '{if($1==var) {print "user:"$1"  uid:"$3"  home:"$6"";exit;}}'
 #awk应该是做了个for循环，从下面这句注释掉额语句的结果可知。
-#cat /etc/passwd|awk -F ":" -v var=$user '{if($1==var) {print "user:"$1"  uid:"$3"  home:"$6"";} else {print ""$1" is not matched."}}'
+#cat /etc/passwd|awk -F ":" -v var=$user '{if($1==var) {print "user:"$1"  uid:"$3"  home:"$6""; exit;} else {print ""$1" is not matched."}}'
