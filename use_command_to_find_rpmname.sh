183c6ae16f89a5c79dbeea0f32968f7c81a3bb0b
王宇铖 <supermaxxx@sina.com>
2015-03-04 15:37:25 +0800

    Update use_command_to_find_rpmname.sh

--- use_command_to_find_rpmname.sh
+++ use_command_to_find_rpmname.sh
@@ -2,9 +2,9 @@
 echo -n "Please enter command: "
 read cmd
 if [ -n "$cmd" ]; then
-rpmpath=`which $cmd`
-echo "RPM_PATH: "$rpmpath
-rpmname=`rpm -qf $rpmpath`
+binpath=`which $cmd`
+echo "BIN_PATH: "$binpath
+rpmname=`rpm -qf $binpath`
 echo "RPM_NAME: "$rpmname
 fi
 
@@ -12,5 +12,5 @@ fi
 ## result:
 # sh use_command_to_find_rpmname.sh
 # Please enter command: df
-# RPM_PATH: /usr/bin/df
+# BIN_PATH: /usr/bin/df
 # RPM_NAME: coreutils-8.22-11.el7.x86_64
