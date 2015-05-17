f084a4115c559fee0b80ac0aa0efe58efc66f6bc
王宇铖 <supermaxxx@sina.com>
2015-05-17 23:38:06 +0800

    Update use_command_to_find_rpmname.sh

--- use_command_to_find_rpmname.sh
+++ use_command_to_find_rpmname.sh
@@ -14,3 +14,7 @@ fi
 # Please enter command: df
 # BIN_PATH: /usr/bin/df
 # RPM_NAME: coreutils-8.22-11.el7.x86_64
+# sh use_command_to_find_rpmname.sh
+# Please enter command: sar
+# BIN_PATH: /usr/bin/sar
+# RPM_NAME: sysstat-9.0.4-27.el6.x86_64
